4b009bdcf3af9d660e2cd48c13cd1b388bf40e40
Mahrozfatima786 <mahrozfatima156@gmail.com>
2025-06-28 16:43:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+    const bands = [
+      'The Plot in You',
+      'The Devil Wears Prada',
+      'Pierce the Veil',
+      'Norma Jean',
+      'The Bled',
+      'Say Anything',
+      'The Midway State',
+      'We Came as Romans',
+      'Counterparts',
+      'Oh, Sleeper',
+      'A Skylit Drive',
+      'Anywhere But Here',
+      'An Old Dog'
+    ];
+
+    // Function to strip leading "a", "an", "the"
+    function stripArticle(name) {
+      return name.replace(/^(a |an |the )/i, '').trim();
+    }
+
+    // Sort by stripped name
+    const sortedBands = bands.sort((a, b) => {
+      return stripArticle(a).localeCompare(stripArticle(b));
+    });
+
+    const ul = document.getElementById('band');
+    ul.innerHTML = sortedBands.map(band => `<li>${band}</li>`).join('');
+  
